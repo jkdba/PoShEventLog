a6c536ad519167c537e38ba9a2361138eca6a160
John Klann <dba@jkdba.com>
2017-05-22 13:15:28 -0400

    spacing

--- PoShEventLog.psm1
+++ PoShEventLog.psm1
@@ -10,7 +10,6 @@ Get-ChildItem -Path $PSScriptRoot -Recurse -File | ForEach-Object {
 
 Export-ModuleMember -Function *
 
-
 # 'Microsoft-Windows-TaskScheduler/Operational'
 # $EQ=Get-EventLogQuery -After '03/31/2017 5:17:00' -Before '04/02/2017 17:37:00' -EventID 201,102
 # Invoke-EventLogQuery -ComputerName name -LogName 'Microsoft-Windows-TaskScheduler/Operational' -LogPathType LogName -Query "$EQ"
